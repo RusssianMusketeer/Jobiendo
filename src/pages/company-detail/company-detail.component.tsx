62ea32bdc301f0725975345c051709118ba89648
Arseni Dmitriev <arseni@Arsenis-MacBook-Pro.local>
2021-11-10 18:17:47 -0500

    added respomsive at job search page

--- src/pages/company-detail/company-detail.component.tsx
+++ src/pages/company-detail/company-detail.component.tsx
@@ -12,6 +12,8 @@ import { useContext } from "react";
 import HomePageContext from '../../context/HomePageContext';
 import { ReactComponent as MapIcon} from '../../assets/map.svg';
 import { useState } from "react";
+import Hamburger from 'hamburger-react';
+import HamburgerMenu from '../../components/hamburger-menu/hamburger-menu.component';
 
 const CompanyDetail = () => {
 
@@ -19,6 +21,7 @@ const CompanyDetail = () => {
     const location:any= useLocation();
     const path = useLocation().pathname;
     const [slice,setSlice]=useState<number[]>([0,4]);
+    const [isOpen, setOpen] = useState(false);
    
 
     return (
@@ -27,6 +30,8 @@ const CompanyDetail = () => {
             <NavLink className="jobiando-mobil-logo" to="/" style={{height:"fit-content", width:"140px",position:"absolute",left:"3rem",top:"3.95rem",zIndex:2}}>
         <NameIcon className="logo-search-list"/>
         </NavLink>
+        <HamburgerMenu info={isOpen}/>
+        <Hamburger toggled={isOpen} toggle={setOpen} size={26}/>
         <div className='header-logo-choice-container'>
              <NavLink className ='choice' to='/'> HOME </NavLink>
              <NavLink className ='choice' to='/search'> JOBS </NavLink>
@@ -35,16 +40,19 @@ const CompanyDetail = () => {
         <div className="company-detail-section">
             <div className="company-detail-title-description">
                 <div className="company-logo-name-div">
+                <div className="flex-mobil-logo">  
                 <div className="company-div-logo-title">
                 <div className="company-logo-detail-div">
                 <img className="company-logo-detail" alt="logo" loading="lazy" src={location.state.company_logo }></img>
-                </div>
+                
                 <div className="company-detail-title-div"><h1 className="company-detail-title">{location.state.title_company}</h1></div>
+                </div>
                 </div>
                 <div className="company-detail-location-industry-size">
                 <div className="company-industry"><p> <SuitCaseIcon/>INDUSTRY</p><span>{location.state.compagny_industry}</span></div>
                 <div className="company-location"><p><LocationIcon/>LOCATION</p><span>{location.state.company_location}</span></div>
                 <div className="company-size"><p><SizeIcon/>SIZE</p><span>{location.state.company_size}</span></div>  
+            </div>
             </div>
             <div className="company-text-description">
             <p>{location.state.company_description !== "TBD" ? location.state.company_description : null}</p>
@@ -60,11 +68,11 @@ const CompanyDetail = () => {
         {location.state.company_job_info.results.length !==0 ?<div style={{width: "80%",margin: "0 auto", marginBottom:"3rem"}}>
         {(loading || location.state.company_job_info.results ===undefined ? ([1,2,3,4,5,].map((n) => <SkeletonListing key={n}/>))
         : (location.state.company_job_info.results.slice(slice[0], slice[1]).map((item:any,index:any)=>(
-        <div className="job-container" style={{marginTop: index===0 ? '3rem' : '2rem', width: "97.4%",cursor:"default"}}>
+        <div className="job-container" style={{marginTop: index===0 ? '3rem' : '2rem', width: "92.4%",cursor:"default"}}>
            
             <div className="upper-part-listing">
-        <div className="title-image">
-        <img className="image-logo" alt="company" loading="lazy" src={location.state.company_logo}></img>
+        <div className="title-image-mobil">
+        <img className="image-logo-mobil" alt="company" loading="lazy" src={location.state.company_logo}></img>
         <div>
         <h1  className="job-title" key={index}>{item.name}</h1>
         <div className="descriptions">
@@ -76,7 +84,7 @@ const CompanyDetail = () => {
         </div>
         </div>
         </div>
-        <div className="button-job-container">
+        <div className="button-job-container-mobil">
        
         <Link className="view-job-button" to={{
             pathname: "./job-detail",
